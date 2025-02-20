df8734f6fe90d905091529bac1b195a81d09c743
INSUP <mini.msmis@gmail.com>
2025-02-21 02:22:06 +0900

    진짜 자고싶다

--- src/main/java/com/kh/controller/KakaoController.java
+++ src/main/java/com/kh/controller/KakaoController.java
@@ -7,8 +7,8 @@ import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.springframework.boot.configurationprocessor.json.JSONException;
-import org.springframework.boot.configurationprocessor.json.JSONObject;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
